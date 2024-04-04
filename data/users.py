c69b637857efa27ee71adf1e28cd7200f546b6ac
Leonid Strukov <leostrukov@ya.ru>
2024-04-04 18:28:49 +0300

    Настроил шаблон главной страницы и создал базы данных

---
+++ data/users.py
@@ -0,0 +1,19 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    news = orm.relationship("News", back_populates='user')
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import db_session
+from data.users import User
+from data.news import News
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'hellow world!'
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != 1)
+    return render_template('news.html', news=news)
+
+
+def craete_news():
+    db_sess = db_session.create_session()
+    news = News(title="Первая моя новость", content="Я так рад что это моя первый пост!!!!!",
+            user_id=2, is_private=False)
+    db_sess.add(news)
+    db_sess.commit()
 
 
 if __name__ == '__main__':
+    db_session.global_init('db/news.db')
+    craete_news()
     app.run(host='127.0.0.1', port=8080)
