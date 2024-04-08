97f802de70aa528dea2dbc10743ca320585f8307
Leonid Strukov <leostrukov@ya.ru>
2024-04-08 23:45:59 +0300

    Поправки

---
+++ tokens.py
@@ -0,0 +1,18 @@
+from itsdangerous import URLSafeTimedSerializer
+import os
+
+
+def generate_token(email):
+    serializer = URLSafeTimedSerializer(os.getenv("SECRET_KEY"))
+    return serializer.dumps(email, salt=os.getenv("SECURITY_PASSWORD_SALT"))
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(os.getenv("SECRET_KEY"))
+    try:
+        email = serializer.loads(
+            token, salt=os.getenv("SECURITY_PASSWORD_SALT"), max_age=expiration
+        )
+        return email
+    except Exception:
+        return False
